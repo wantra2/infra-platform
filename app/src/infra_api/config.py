2717969c7cbff30f0acb34d272c580ffb9e81fd3
wantra2 <denis.berenger@epita.fr>
2026-08-18 20:33:24 +0200

    Improve dockerfile caching

--- app/src/infra_api/config.py
+++ app/src/infra_api/config.py
@@ -1,12 +1,12 @@
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
-#automatically reading environment variables if defined, else find it in .devenv
+#automatically reading environment variables if defined, else find it in .dev.env
 class Settings(BaseSettings):
     database_url: str
     environment: str = "development"
 
     model_config = SettingsConfigDict(
-        env_file=".devenv",
+        env_file=".dev.env",
         env_file_encoding="utf-8",
         extra="ignore",
     )
